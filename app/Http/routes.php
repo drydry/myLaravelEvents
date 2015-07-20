b2f23fbadf77b9895e03c9ed3e9519d1a0fec91b
drydry <cedric.pommepuy@gmail.com>
2015-07-20 19:17:44 -0400

    updated route to display welcome page + defined the view

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,10 @@
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 // Authentication routes...
 Route::get('auth/login', 'Auth\AuthController@getLogin');
 Route::post('auth/login', 'Auth\AuthController@postLogin');
